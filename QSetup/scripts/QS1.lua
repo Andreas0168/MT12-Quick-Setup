0aa61bf620edef632bca3b190d3742dd01cc1eea
Andreas Kassner <a.kassner@live.de>
2026-09-22 23:50:46 +0200

    Update QS1.lua

--- QSetup/scripts/QS1.lua
+++ QSetup/scripts/QS1.lua
@@ -75,25 +75,20 @@ local function display(groupNum, event, siteNum, iSel, lg, editMode, lcdCnt, chn
 		if t ~= glVars[n] then fld = n val = t end
 		glVars[n] = t
 	end
-	if fld == 0 then qs_setPopup({getText(32), val..'%'}) end
-	if fld == 1 then
-		qs_setPopup({getText(33), (val < 0 and 'L ' or val > 0 and 'R ' or '') ..
-		string.format('%.1f', (val < 0 and -val or val) * .5) .. '%'})
-	end
 	if valSrcThr > -19 and valSrcThr < 19 then
+		if fld == 0 then qs_setPopup({getText(32), val..'%'}) end
+		if fld == 1 then
+			qs_setPopup({getText(33), (val < 0 and 'L ' or val > 0 and 'R ' or '') ..
+			string.format('%.1f', (val < 0 and -val or val) * .5) .. '%'})
+		end
 		if fld == 2 then qs_setPopup({getText(34), val..'%'}) end
 		if fld == 3 then qs_setPopup({getText(35), val..'%'}) end
 		if fld == 4 then
 			qs_setPopup({getText(19), (val < 0 and getText(20) or val > 0 and getText(21) or '')..
-			' '..(val < 0 and -val or val) * 5 ..'%'})
+			' '..string.format('%.1f', (val < 0 and -val or val) * .5) .. '%'})
 			qs_init(1)
 		end
 	end
-
---	if fld == 4 then
---		qs_setPopup({getText(19), (val < 0 and getText(20) or val > 0 and getText(21) or '')..
---		' '..string.format('%.1f', (val < 0 and -val or val) * .5)..'%'})
---	end
 
 	lcdCnt = 0
 
